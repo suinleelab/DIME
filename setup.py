0c524822af93697c604543416735a5d36c6cab26
Soham Gadgil <sgadgil@cs.washington.edu>
2023-06-15 13:18:07 -0700

    adding dependencies to setup

--- setup.py
+++ setup.py
@@ -12,6 +12,22 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='',
     packages=['dime', 'experiments', 'baseline_models'],
+    install_requires=[
+        'numpy',
+        'fastai>=2.5.3',
+        'kaggle>=1.5.13',
+        'opencv-python',
+        'pandas',
+        'scikit-learn',
+        'tensorboard>=2.10.1',
+        'timm>=0.6.12',
+        'torch>=1.13.1',
+        'torchvision>=0.14.1',
+        'tqdm',
+        'pandas>=1.5.2',
+        'torchmetrics>=0.11.0',
+        'pytorch_lightning>=1.8.6'
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
