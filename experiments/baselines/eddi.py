fc2beaba3172413ec07dc9addabb2c118d831298
Soham Gadgil <sgadgil@cs.washington.edu>
2023-07-14 16:46:04 -0700

    updating imports

--- experiments/baselines/eddi.py
+++ experiments/baselines/eddi.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from tqdm import tqdm
-from dime.data_utils import MaskLayerGrouped
+from dime.utils import MaskLayerGrouped
 from baselines.iterative import calculate_criterion, Imputer
 
 
